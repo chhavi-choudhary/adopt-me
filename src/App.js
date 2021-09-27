618c49711b815bf0d6b77a4b8b3253fcd3939534
Chhavi <Chhavi.choudhary75@gmail.com>
2021-09-27 05:50:25 +0200

    eslint-parcel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
+  return React.createElement("div", {}, [
+    React.createElement("h1", "Adopt Me!"),
     React.createElement(Pet, {
       name: "Luna",
       animal: "Dog",
@@ -25,7 +18,7 @@ const App = () => {
       name: "Sudo",
       animal: "Dog",
       breed: "Wheaten Terrier",
-    })
-  );
+    }),
+  ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
